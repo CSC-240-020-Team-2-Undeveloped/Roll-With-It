1d2a3d730b470c85d8ab82424d65e3aa7876029c
Kody Knight <kody.brandon.knight@gmail.com>
2023-04-22 15:43:02 -0400

    Audio changes
    1. changed jumping script to play sound on jump
    2. added a audio file to the game
    3. connected said file to audio listener on jumping script

--- Assets/Code/JumpingCode.cs
+++ Assets/Code/JumpingCode.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class JumpingCode : MonoBehaviour
 {
-    public Vector3 jump;
+    private Vector3 jump;
     public float jumpForce = 2.0f;
+    public AudioSource AudioS;
     
     public bool isGrounded;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
+	  AudioS = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
@@ -27,6 +29,7 @@ public class JumpingCode : MonoBehaviour
          if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
 
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+		AudioS.Play();
             isGrounded = false;
         }
     }
